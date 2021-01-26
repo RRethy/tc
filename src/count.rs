1d518580ad08d247ab46f4a83016bbf8872dc656
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2021-01-26 12:51:31 -0500

    Add an implementation that uses a fixed buffer to read the file
    valgrind tc:
        561,422 bytes allocated
    time tc:
        0.02s user 0.01s system 84% cpu 0.026 total
    
    valgrind wc:
        1,192,502 bytes allocated
    time wc:
        0.05s user 0.01s system 88% cpu 0.063 total

--- src/count.rs
+++ src/count.rs
@@ -1,5 +1,9 @@
 use crate::config::Config;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
+
+const BUFFER_SIZE: usize = 512 * 1024;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Context<'a> {
@@ -14,36 +18,73 @@ pub struct Count<'a> {
     lines: Option<usize>,
 }
 
-pub fn files<'a>(files: &'a Vec<PathBuf>, config: &Config) -> Vec<Count<'a>> {
-    files.into_iter().map(|f| file(f, config)).collect()
+pub fn files<'a>(paths: &'a Vec<PathBuf>, config: &Config) -> Vec<Count<'a>> {
+    paths.into_iter().map(|path| file(path, config)).collect()
 }
 
-pub fn file<'a>(f: &'a PathBuf, config: &Config) -> Count<'a> {
-    let text = &std::fs::read(&f).unwrap();
+pub fn file<'a>(path: &'a PathBuf, config: &Config) -> Count<'a> {
+    let file = File::open(path).unwrap();
+    let mut reader = BufReader::with_capacity(BUFFER_SIZE, file);
     let mut lines = 0;
-    let bytes = text.len();
+    let mut bytes = 0;
     let mut words = 0;
     let mut in_word = false;
-    for &b in text {
-        if b == b'\n' {
-            lines += 1;
+    loop {
+        let buffer = reader.fill_buf().unwrap();
+        let l = buffer.len();
+        if l == 0 {
+            break;
         }
-        if b.is_ascii_whitespace() {
-            if in_word {
-                words += 1;
-                in_word = false;
+        bytes += l;
+        for &b in buffer {
+            if b == b'\n' {
+                lines += 1;
             }
-        } else {
-            in_word = true;
+            if b.is_ascii_whitespace() {
+                if in_word {
+                    words += 1;
+                    in_word = false;
+                }
+            } else {
+                in_word = true;
+            }
         }
+        reader.consume(l);
     }
     Count {
-        context: Context::File { path: &f },
+        context: Context::File { path },
         bytes: if config.bytes { Some(bytes) } else { None },
         words: if config.words { Some(words) } else { None },
         lines: if config.lines { Some(lines) } else { None },
     }
 }
+
+// pub fn file<'a>(f: &'a PathBuf, config: &Config) -> Count<'a> {
+//     let text = &std::fs::read(&f).unwrap();
+//     let mut lines = 0;
+//     let bytes = text.len();
+//     let mut words = 0;
+//     let mut in_word = false;
+//     for &b in text {
+//         if b == b'\n' {
+//             lines += 1;
+//         }
+//         if b.is_ascii_whitespace() {
+//             if in_word {
+//                 words += 1;
+//                 in_word = false;
+//             }
+//         } else {
+//             in_word = true;
+//         }
+//     }
+//     Count {
+//         context: Context::File { path: &f },
+//         bytes: if config.bytes { Some(bytes) } else { None },
+//         words: if config.words { Some(words) } else { None },
+//         lines: if config.lines { Some(lines) } else { None },
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
